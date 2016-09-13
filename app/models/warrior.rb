3035d1c6e5472bbb4c12c345f64d05363995ec70
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-09-12 22:45:15 -0500

    move caluclation method to attackcalculator class

--- app/models/warrior.rb
+++ app/models/warrior.rb
@@ -1,20 +1,22 @@
+require_relative 'attack_calculator'
+
 class Warrior
 
   attr_reader :level, :strength, :vitality, :attacks
 
   def initialize(args = {})
     @level = args.fetch(:level, 1)
-    @strength = args.fetch(:strength, total_strength(@level))
-    @vitality = args.fetch(:vitality, total_vitality(@level))
+    @strength = args.fetch(:strength, 0)
+    @vitality = args.fetch(:vitality,0)
     @attacks = self.class::KNOWN_ATTACKS
   end
 
-  def total_strength(level)
-    level * self.class::STRENGTH_INCREASE
-  end
-
-  def total_vitality(level)
-    level * self.class::VITALITY_INCREASE
-  end
+  # def total_strength(level)
+  #   level * self.class::STRENGTH_INCREASE
+  # end
+  #
+  # def total_vitality(level)
+  #   level * self.class::VITALITY_INCREASE
+  # end
 
 end
