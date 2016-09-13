52267b71592ad57895cf94eca51564f7811c9ded
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-09-12 21:30:47 -0500

    att attacks to lena

--- app/models/lena.rb
+++ app/models/lena.rb
@@ -1,9 +1,11 @@
 require_relative 'warrior'
+require_relative 'punch'
 
 class Lena < Warrior
 
   STRENGTH_INCREASE = 10
   VITALITY_INCREASE = 5
+  KNOWN_ATTACKS = [Punch.new, Smite.new, Cleave.new]
 
   def initialize(level)
     super
