ae3ee83beefab6707ad5640293a218c2c09d0abc
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-09-12 21:13:53 -0500

    mass past initial tests

---
+++ app/models/mace.rb
@@ -0,0 +1,14 @@
+class Mace
+
+  STANDARD_APS = 1
+  attr_reader :min_dmg, :max_dmg, :element_boost, :str_modifer, :aps
+  
+  def initialize(args = {})
+    @min_dmg = args.fetch(:min_dmg, 0)
+    @max_dmg = args.fetch(:max_dmg, 0)
+    @element_boost = args.fetch(:element_boost, nil)
+    @str_modifer = args.fetch(:str_modifer, 0)
+    @aps = STANDARD_APS
+  end
+
+end
