a0ebd881b3f377ed2a369d5430ea79cd690d7f24
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-09-12 20:46:11 -0500

    basic lena tests

---
+++ spec/models/lena_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../../app/models/lena'
+
+RSpec.describe Lena do
+
+  let(:lena) { Lena.new(level: 10) }
+
+  describe 'attributes' do
+    it 'has a level' do
+      expect(lena.level).to eq 10
+    end
+
+    it 'has vitality as integer' do
+      expect(lena.vitality).to eq 50
+    end
+
+    it 'has strength as integer' do
+      expect(lena.strength).to eq 100
+    end
+    
+  end
+end
