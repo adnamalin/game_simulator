026a496f382b8189db02bdda93a79cf5ef866fc0
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-10-08 21:10:05 -0500

    simplify weapon and warrior matcher with string to class method

--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -1,16 +1,20 @@
 helpers do
 
-  def weapon_creator(weapon_choice, params)
-    details = details_mapper(params)
-    weapon_matcher = {
-      "Axe" => Axe,
-      "Mace" => Mace,
-      "Sword" => Sword
-    }
-    weapon_matcher[weapon_choice].new(details)
+  def warrior_creator(params)
+    details = weapon_details_mapper(params)
+    Object.const_get(params[:warrior]).new(details)
   end
 
-  def details_mapper(original_params)
+  def weapon_details_mapper(original_params)
+    {level: params[:level].to_i}
+  end
+
+  def weapon_creator(params)
+    details = weapon_details_mapper(params)
+    Object.const_get(params[:weapon]).new(details)
+  end
+
+  def weapon_details_mapper(original_params)
     percent = percent_to_float_convertor(original_params[:element_boost_percent])
     {
       min_dmg: params[:min_damage].to_i,
