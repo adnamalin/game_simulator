5a8ea8983228454ee2a67355f442550a11f3a7c0
Amanda Lin <amandalin@TCHQ-6396-Amanda-Lin-MBP.local>
2016-10-12 10:46:49 -0500

    attackcalculator back to class, created #calculate_all method as only public method

--- app/models/attack_calculator.rb
+++ app/models/attack_calculator.rb
@@ -1,53 +1,66 @@
-module AttackCalculator
+class AttackCalculator
+    class << self
 
-    def calculate_max_attack(warrior, weapon, attack)
-      base_attack = attack_dmg_percentage(attack) * weapon.max_dmg
-      calculate_attack(warrior, weapon,attack, base_attack)
-    end
+      def calculate_all(warrior, weapon, attack)
+        min = calculate_min_attack(warrior, weapon, attack)
+        max = calculate_max_attack(warrior, weapon, attack)
+        dps = damage_per_second(min, max, weapon, attack)
+        {attack: attack.class.name ,minimum: min, maximum: max, DPS: dps}
+      end
 
-    def calculate_min_attack(warrior, weapon, attack)
-      base_attack = attack_dmg_percentage(attack) * weapon.min_dmg
-      calculate_attack(warrior, weapon, attack, base_attack)
-    end
+      private
 
-    def calculate_attack(warrior, weapon, attack, base_attack)
-      no_boost_total = no_boost_total_calculator(warrior, weapon, base_attack)
-      final_total = boost_increase_checker(no_boost_total, weapon, attack)
-    end
+      def calculate_max_attack(warrior, weapon, attack)
+        base_attack = attack_dmg_percentage(attack) * weapon.max_dmg
+        calculate_attack(warrior, weapon,attack, base_attack)
+      end
 
-    def no_boost_total_calculator(warrior, weapon, base_attack)
-      strength_multiplier = strength_multiplier_calculator(warrior, weapon)
-      (base_attack * strength_multiplier) + base_attack
-    end
+      def calculate_min_attack(warrior, weapon, attack)
+        base_attack = attack_dmg_percentage(attack) * weapon.min_dmg
+        calculate_attack(warrior, weapon, attack, base_attack)
+      end
 
-    def strength_multiplier_calculator(warrior, weapon)
-      (warrior.strength + weapon.str_modifer)/100.to_f
-    end
+      def calculate_attack(warrior, weapon, attack, base_attack)
+        no_boost_total = no_boost_total_calculator(warrior, weapon, base_attack)
+        boost_increase_checker(no_boost_total, weapon, attack)
+      end
 
-    def boost_increase_checker(no_boost_total, weapon, attack)
-      if boost_increase_qualified?(weapon, attack)
-        boost_increase_adder(no_boost_total, weapon)
-      else
-        no_boost_total
+      def no_boost_total_calculator(warrior, weapon, base_attack)
+        strength_multiplier = strength_multiplier_calculator(warrior, weapon)
+        (base_attack * strength_multiplier) + base_attack
+      end
+
+      def strength_multiplier_calculator(warrior, weapon)
+        (warrior.strength + weapon.str_modifer)/100.to_f
+      end
+
+      def boost_increase_checker(no_boost_total, weapon, attack)
+        if boost_increase_qualified?(weapon, attack)
+          boost_increase_adder(no_boost_total, weapon)
+        else
+          no_boost_total
+        end
+      end
+
+      def boost_increase_qualified?(weapon, attack)
+        weapon.element_type == attack.element_type
+      end
+
+      def boost_increase_adder(no_boost_total, weapon)
+        no_boost_total + (no_boost_total * weapon.element_boost_amt)
+      end
+
+      def damage_per_second(min_atk, max_atk, weapon, attack)
+        base_aps = average_atk(min_atk, max_atk) * weapon.aps
+        (base_aps * attack.aps_percentage).round(3)
+      end
+
+      def average_atk(min_atk, max_atk)
+        (min_atk + max_atk)/2
+      end
+
+      def attack_dmg_percentage(attack)
+        attack.dmg_percentage
       end
     end
-
-    def boost_increase_qualified?(weapon, attack)
-      weapon.element_type == attack.element_type
-    end
-
-    def boost_increase_adder(no_boost_total, weapon)
-      no_boost_total + (no_boost_total * weapon.element_boost_amt)
-    end
-
-    def damage_per_second(attack_dmg ={}, weapon, attack)
-      average_atk = (attack_dmg[:min] + attack_dmg[:max])/2
-      base_aps = average_atk * weapon.aps
-      (base_aps * attack.aps_percentage).round(3)
-    end
-
-    def attack_dmg_percentage(attack)
-      attack.dmg_percentage
-    end
-
 end
--- spec/models/attack_calculator_spec.rb
+++ spec/models/attack_calculator_spec.rb
@@ -3,17 +3,26 @@ require_relative '../../app/models/axe'
 require_relative '../../app/models/punch'
 require_relative '../../app/models/cleave'
 require_relative '../../app/models/smite'
+require_relative '../../app/models/attack_calculator'
 
 RSpec.describe AttackCalculator do
 
-  let(:calculator) { Class.new { extend AttackCalculator } }
+  let(:calculator) { AttackCalculator }
   let(:lena) { Lena.new({level: 10}) }
   let(:axe) { Axe.new({min_dmg: 50, max_dmg: 65, element_boost_amt: 0.10, element_type: :fire, str_modifer: 20} )}
-  let(:punch) { Punch.new()}
-  let(:cleave) { Cleave.new()}
-  let(:smite) { Smite.new()}
+  let(:punch) { Punch.new }
+  let(:cleave) { Cleave.new }
+  let(:smite) { Smite.new }
 
-  describe '#calculate_attack for min damage' do
+  describe '#calculate_all' do
+
+    it 'returns hash of attack name, min attack, max attack, and dps values' do
+      expect(calculator.calculate_all(lena, axe, punch)).to eq({attack: "Punch", minimum: 60, maximum: 78, DPS: 82.8})
+    end
+
+  end
+
+  xdescribe '#calculate_attack for min damage' do
 
     it 'returns the min attack dmg done punch' do
       expect(calculator.calculate_min_attack(lena, axe, punch)).to eq 60
@@ -29,7 +38,7 @@ RSpec.describe AttackCalculator do
 
   end
 
-  describe '#calculate_attack for max damage' do
+  xdescribe '#calculate_attack for max damage' do
 
     it 'returns the max attack dmg done punch' do
       expect(calculator.calculate_max_attack(lena, axe, punch)).to eq 78
@@ -45,7 +54,7 @@ RSpec.describe AttackCalculator do
   end
 
 
-  describe '#boost_increase_checker' do
+  xdescribe '#boost_increase_checker' do
     it 'returns the total damage if the attack and weaon match element types' do
       expect(calculator.boost_increase_checker(54, axe, smite)).to eq 59.4
     end
@@ -56,18 +65,18 @@ RSpec.describe AttackCalculator do
 
   end
 
-  describe '#damage_per_second' do
+  xdescribe '#damage_per_second' do
 
     it 'returns the dmg per second for punch' do
-      expect(calculator.damage_per_second({min: 60, max:78},axe, punch)).to eq 82.8
+      expect(calculator.damage_per_second( 60, 78 , axe, punch)).to eq 82.8
     end
 
     it 'returns the dmg per second for smite' do
-      expect(calculator.damage_per_second({min: 59.4, max:77.22},axe, smite)).to eq 98.366
+      expect(calculator.damage_per_second( 59.4, 77.22 , axe, smite)).to eq 98.366
     end
 
     it 'returns the dmg per second for cleave' do
-      expect(calculator.damage_per_second({min: 72, max:93.6},axe, cleave)).to eq 89.424
+      expect(calculator.damage_per_second( 72, 93.6 , axe, cleave)).to eq 89.424
     end
 
   end
